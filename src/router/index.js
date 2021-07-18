96dca226764387bb89ccbedb961f08f8dcdccbb8
Osmi Santiago Otalora Guerrero <santiago.ooo6@gmail.com>
2021-07-18 10:52:28 -0500

    startin app pokemon

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,56 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '../store'
+
+const LandingPokemons = () => import('../views/LandingPokemons.vue')
+const Pokemon = () => import('../views/Pokemon.vue')
+const Login = () => import('../views/Login.vue')
+const Register = () => import('../views/Register.vue')
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Landing',
+    component: LandingPokemons
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/pokemon/:id',
+    name: 'Pokemon',
+    props: true,
+    component: Pokemon,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  // Requires auth & no user
+  if (requiresAuth && !(await store.dispatch('user/getCurrentUser'))) {
+    next({ name: 'auth' })
+    // No requires auth and user (auth)
+  } else if (!requiresAuth && (await store.dispatch('user/getCurrentUser'))) {
+    next({ name: 'Home' })
+  } else {
+    // Anything else
+    next()
+  }
 })
 
 export default router
---
+++ src/views/LandingPokemons.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="landing">
+    <div >
+      <ItemPokemon name="" url=""/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ItemPokemon from '@/components/ItemPokemon.vue'
+
+export default {
+  name: 'LandingPokemons',
+  components: {
+    ItemPokemon
+  },
+  data: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
